45afbaf12893f18a2e417d6e393fcb85ac331917
geekhunger <jack0088@me.com>
2022-01-12 17:21:48 +0000

    first real commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Why?
+
+[HarperDB](https://harperdb.io) is a distributed realtime JSON cloud database. It's highly scalable, has low-latency and is super easy to use and maintain. There are even build-in cloud functions, loadbalancing and other great goodies! Their free tier service plan is certainly capable of running decent applications too. But, their [HTTP API](https://api.harperdb.io) is not a turnkey solution.
+
+<p align="center"><img src="harperdb.png" height="64"></p>
+
+
+# node-harperdb
+
+Coming soon...
--- harperdb.js
+++ harperdb.js
@@ -1,76 +1,53 @@
-const ROOTPATH = require("app-root-path")
-const share = ROOTPATH.require("share")
-const {PACKAGE, CREDENTIALS} = share
-const {typecheck, assert} = share.fn
 const request = require("needle")
-
-
-request.defaults({
-    headers: {
-        "User-Agent": PACKAGE.name,
-        "Cache-Control": "no-cache",
-        "Authorization": "Basic " + CREDENTIALS.harperdb.token,
-        "Accept": "application/json"
-    },
-    parse_response: "json",
-    timeout: CREDENTIALS.harperdb.timeout
-})
-
-
-const valid_json = function(obj) {
+const valid_record = input => Array.isArray(input) || typeof input === "string" ? input : [input]
+const valid_json = obj => {
     try {return JSON.stringify(obj)}
     catch(error) {return null}
 }
 
 
-const valid_record = function(input) {
-    return Array.isArray(input) || typeof input === "string" ? input : [input]
-}
-
-
-const post = async function(query) {
-    const response = (await request(
-        "POST",
-        CREDENTIALS.harperdb.url,
-        typeof query === "string" ? valid_json({operation: "sql", sql: query}) : valid_json(query),
-        {json: true}
-    )).body
-    if(response.error) {
-        throw new Error(response.error)
-    }
-    return response
-}
-
-
-/*
-    HarperDB connector
-    Class instances 'mount' to a db schema (namespace) and a table to runs queries on them
-    Fallowing examples do the same:
-        const db = new HarperDB("dev", "blog_posts")
-        const db = new HarperDB("dev.blog_posts") // argument will be split into [schema, table]
-        db.insert({title: "hello world", content: "welcome to my cool blog"})
-        db.upsert({title: "hello world", content: "welcome to my cool blog"})
-        const post_id = db.run(`select * from ${db.schema}.${db.table} where title is like 'hello%'`)
-        db.delete(post_id)
-*/
 class HarperDB {
-    constructor(schema, table) {
-        if(typeof schema !== "string") throw new Error("Schema not specified")
-        if(typeof table !== "string") [schema, table] = schema.split(".")
-        if(typeof table !== "string") throw new Error("Table not specified")
+    /*
+        HarperDB connector
+        Class instances 'mount' onto a db schema (namespace) and table to run queries on them
+    */
+    constructor(instance, auth, schema, table) {
+        this.instance = instance
+        this.auth = auth
         this.schema = schema
         this.table = table
         this.schema_undefined = this.table_undefined = true
     }
 
-    /*
-        Argument can be any valid SQL query string
-        or a command object accepted by HarperDB
-        NOTE The first call will create missing schema and/or table recursevly!
-    */
+
+    async request(query) {
+        const payload = typeof query === "string"
+            ? valid_json({operation: "sql", sql: query})
+            : valid_json(query)
+        const settings = {
+            headers: {
+                "Accept": "application/json",
+                "Cache-Control": "no-cache",
+                "Authorization": "Basic " + this.auth
+            },
+            json: true,
+            parse: "json",
+            timeout: 15000 // ms
+        }
+        const response = (await request("post", this.instance, payload, settings))
+        if(response?.body?.error) throw new Error(response.body.error)
+        return response?.body
+    }
+
+
     async run(query) {
+        /*
+            Argument can be any valid SQL query string
+            or a command object accepted by HarperDB
+            NOTE The first call will create missing schema and/or table recursevly!
+        */
         try {
-            const response = await post(query)
+            const response = await this.request(query)
             this.schema_undefined = this.table_undefined = false
             return response
         } catch(error) {
@@ -80,12 +57,12 @@ class HarperDB {
             let schema
             if(this.schema_undefined) { // prepare schema
                 try {
-                    schema = await post({
+                    schema = await this.request({
                         operation: "describe_schema",
                         schema: this.schema
                     })
-                } catch(quiet) {
-                    await post({
+                } catch(_) {
+                    await this.request({
                         operation: "create_schema",
                         schema: this.schema
                     }).catch(() => {})
@@ -96,7 +73,7 @@ class HarperDB {
             if(this.table_undefined) { // prepare table
                 try {
                     if(!schema) {
-                        await post({
+                        await this.request({
                             operation: "describe_table",
                             schema: this.schema,
                             table: this.table
@@ -104,8 +81,8 @@ class HarperDB {
                     } else if(!schema[this.table]) {
                         throw new Error(`Table '${this.table}' not defined`)
                     }
-                } catch(quiet) {
-                    await post({
+                } catch(_) {
+                    await this.request({
                         operation: "create_table",
                         schema: this.schema,
                         table: this.table,
@@ -116,7 +93,7 @@ class HarperDB {
                 }
             }
         }
-        return await post(query) // retry
+        return await this.request(query) // retry
     }
 
 
@@ -143,14 +120,14 @@ class HarperDB {
     async upsert(record) { // can be a single object or an array
         record = valid_record(record)
         try { // check if payload records contain primary keys, otherwise find their primary keys and use then in the request, such that we don't trigger creation of new records in the database
-            const schema = await post({
+            const schema = await this.request({
                 operation: "describe_table",
                 schema: this.schema,
                 table: this.table
             })
             const primary_key = schema.hash_attribute
             for(let new_item of record) {
-                if(typecheck({nil: new_item[primary_key]}, false)) {
+                if(!new_item[primary_key]) {
                     const db_entry = await this.select(new_item)
                     switch(db_entry.length) {
                         case 0: break; // no primary key available because does not exist in db yet!
@@ -159,7 +136,7 @@ class HarperDB {
                     }
                 }
             }
-        } catch(quiet) {
+        } catch(_) {
         } finally {
             return await this.run({
                 operation: "upsert",
@@ -177,13 +154,13 @@ class HarperDB {
             schema: this.schema,
             table: this.table
         })
-        if(typecheck({nil: filter}, false)) { // The search result will be a table schema description if no filtering options have been set, for example: db.select()
+        if(!filter) { // The search result will be a table schema description if no filtering options have been set, for example: db.select()
             //console.info(`Selecting records from database without filtering options will return the table schema instead of a search result!`)
             return table
         }
         let match
         const conditions = []
-        if(typecheck({object: filter}, false)) { // The search result will be an exact match of all attributes and values from the filtering options, for example: db.select({username: "geekhunger", email: "hallo@geekhunger.de"})
+        if(typeof filter === "object" && !Array.isArray(filter)) { // The search result will be an exact match of all attributes and values from the filtering options, for example: db.select({username: "geekhunger", email: "hallo@geekhunger.de"})
             match = "and"
             for(const [attr, val] of Object.entries(filter)) {
                 conditions.push({
@@ -193,7 +170,9 @@ class HarperDB {
                 })
             }
         } else {
-            assert(typecheck({array: filter}, false), "Could not select records from database because filtering options are malformed! Filter must be either nothing, an object of attributes and values for an exact match, or an array of values to search for.")
+            if(!Array.isArray(filter)) {
+                throw new Error("Could not select records from database because filtering options are malformed! Filter must be either nothing, an object of attributes and values for an exact match, or an array of values to search for.")
+            }
             for(const attr of table.attributes.map(attr => attr.attribute)) { // collect all available attributes from table schema and create one search query for every attribute and value combination
                 for(const val of filter) {
                     conditions.push({
@@ -227,8 +206,8 @@ class HarperDB {
     }
 
 
-    pipe(request, ...params) {
-        assert(typeof request === "function", "Could not batch request without any handler function!")
+    pipe(request, ...params) { // queue database operations to execute then later all at once
+        if(typeof request !== "function") throw new Error("Could not batch request without any handler function!")
         if(!Array.isArray(this.pipeline)) this.pipeline = [] // init queue
         this.pipeline.push(Promise.resolve(request.call(this, ...params))) // https://stackoverflow.com/q/60980357/4383587
     }
@@ -243,7 +222,27 @@ class HarperDB {
 
 
 module.exports = {
-    Database: HarperDB,
-    mount: (...arg) => new HarperDB(...arg), // alias for swapping namespaces (omit 'new' keyword for class instances)
-    run: post // alias for running sql queries
+    database: (instance, auth) => {
+        if(!(instance && auth) && !!this.db) {
+            return this.db
+        }
+        if((typeof instance !== "string" || typeof auth !== "string") && !this.db) {
+            throw new Error("Credentials invalid!")
+        }
+        this.db = new HarperDB(instance, auth, this.db?.schema, this.db?.table)
+        return this.db
+    },
+
+    mount: (schema, table) => { // alias for swapping namespaces (omit 'new' keyword for class instances)
+        if(typeof schema !== "string") throw new Error("Schema not specified")
+        if(typeof table !== "string") [schema, table] = schema.split(".") // support for object-like name chaining: "schema.table"
+        if(typeof table !== "string") throw new Error("Table not specified")
+        this.db = new HarperDB(this.db?.instance, this.db?.auth, schema, table)
+        return this.db
+    },
+
+    run: query => { // more intuitive alias for running sql queries
+        if(!this.db) throw new Error("Connection invalid!")
+        return this.db?.run(query)
+    }
 }
